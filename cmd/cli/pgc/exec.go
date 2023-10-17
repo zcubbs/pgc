d4cebe10445ee5636935a07e4023316274c043d3
zcubbs <zackcubby@gmail.com>
2023-10-17 10:49:46 +0200

    🌱 handle entry already exists err

--- cmd/cli/pgc/exec.go
+++ cmd/cli/pgc/exec.go
@@ -3,11 +3,16 @@ package pgc
 import (
 	"database/sql"
 	"fmt"
+	"log"
 )
 
 func CreateDatabase(db *sql.DB, dbName, owner string) error {
 	_, err := db.Exec(fmt.Sprintf("CREATE DATABASE %s WITH OWNER %s;", dbName, owner))
 	if err != nil {
+		if err.Error() == fmt.Sprintf("pq: database \"%s\" already exists", dbName) {
+			log.Printf("database %s already exists, skipping...", dbName)
+			return nil
+		}
 		return fmt.Errorf("error creating database: %w", err)
 	}
 	return nil
@@ -16,6 +21,10 @@ func CreateDatabase(db *sql.DB, dbName, owner string) error {
 func CreateUser(db *sql.DB, username, password string) error {
 	_, err := db.Exec(fmt.Sprintf("CREATE USER %s WITH ENCRYPTED PASSWORD '%s';", username, password))
 	if err != nil {
+		if err.Error() == fmt.Sprintf("pq: role \"%s\" already exists", username) {
+			log.Printf("user %s already exists, skipping...", username)
+			return nil
+		}
 		return fmt.Errorf("error creating user: %w", err)
 	}
 	return nil
@@ -24,6 +33,10 @@ func CreateUser(db *sql.DB, username, password string) error {
 func GrantPrivileges(db *sql.DB, dbName, username, privileges string) error {
 	_, err := db.Exec(fmt.Sprintf("GRANT %s ON DATABASE %s TO %s;", privileges, dbName, username))
 	if err != nil {
+		if err.Error() == fmt.Sprintf("pq: role \"%s\" does not exist", username) {
+			log.Printf("user %s does not exist, skipping...", username)
+			return nil
+		}
 		return fmt.Errorf("error granting privileges: %w", err)
 	}
 	return nil
